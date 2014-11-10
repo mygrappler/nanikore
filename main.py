fed791c8c8738178863fa464ec32de2a4f96e50e
Grappler <asdfg12535@gmail.com>
2014-11-10 23:48:02 +0900

    nanikoree
    test

--- main.py
+++ main.py
@@ -11,10 +11,14 @@ Vb = [10.01, 10.05, 10.10, 10.30, 10.50, 11.00, 12.00, 15.00, 20.00]
 
 
 for var in range(0, len(Va)):
+    if (Va[var] == 0):
+        print(0)
     vH = V0 - Va[var]
     vAll = Va[var] + V0
     V1 = round_sig(((vH*0.001)/vAll) * 100, 4)
     print(round_sig(-math.log10(V1), 4))
+    if(Va[var] == 8):
+        print(7)
 
 for var in range(0, len(Vb)):
     vH = Vb[var] - V0
